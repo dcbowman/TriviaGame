c24798ae7f88da554fd6c3741c7802fb0f56460f
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-01 23:02:22 -0400

    added start and scorehtml

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
+//start game
+
+//click button to start timer and reveal questions
+
+//set timer for 75 seconds
+
+//time's up ends game and shows score page
+
+
+//put those into two categories
+
 var cat1 = ($("input[@name=q1]:checked").val() != "a");
 
 var cat2 = ($("input[@name=q2]:checked").val() != "b");
+
+//check if player selected the right category
+
+//if they selected the right one, increase the correct
+
+//else increase the incorrect
+
